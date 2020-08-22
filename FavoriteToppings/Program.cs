6924e78dbea72c186c886eb97e701d9c64e736d5
Nathan Gonzalez <ncgonz@gmail.com>
2020-08-22 09:49:43 -0500

    Add project files.

---
+++ FavoriteToppings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FavoriteToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText("pizzas.json");
+
+            var pizzas = JsonSerializer.Deserialize<List<Pizza>>(json);
+
+            var twoToppingPizzas = pizzas.Where(p => p.toppings.Count == 2);
+
+        }
+    }
+
+
+    public class Pizza    {
+        public List<string> toppings { get; set; } 
+    }
+}
